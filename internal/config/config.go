36e12b2718e3c251060f93a1e3ca2d2c3d1cc171
Jaro Heinonen <heinonenjaro@proton.me>
2025-08-31 19:57:52 +0300

    feat: add a config package

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"log"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Spotify struct {
+		// This can be left as is, and it should work.
+		// If propblems occur then change the port to something else
+		Redirect string `envconfig:"SPOTIFY_REDIRECT" validate:"required,url"`
+
+		// Comes from the Spotify API application that you generate yourself
+		// https://developer.spotify.com/documentation/web-api
+		ID     string `envconfig:"SPOTIFY_ID" validate:"required"`
+		Secret string `envconfig:"SPOTIFY_SECRET" validate:"required"`
+
+		// ISO 3166-1 alpha-2 country code
+		// https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+		Country string `envconfig:"COUNTRY" validate:"required,len=2"`
+	}
+}
+
+func Load() *Config {
+	cfg := &Config{}
+
+	// Read from environment variables
+	err := readEnv(cfg)
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(cfg); err != nil {
+		log.Panicf("Configuration validation failed: %s", err)
+	}
+
+	log.Printf("%+v", cfg)
+
+	return cfg
+}
+
+func readEnv(cfg *Config) error {
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
